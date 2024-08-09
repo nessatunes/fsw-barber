01bfe335f30de8c361e4e0aadc4eb7433d712672
Nessatunes <essatunes@gmail.com>
2024-08-09 20:50:24 -0300

    feat: ensure user can search for barbershop

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ import { db } from "./_lib/prisma"
 import BarbershopItem from "./_components/barbershop-item"
 import { quickSearchOptions } from "./_constants/search"
 import BookingItem from "./_components/booking-item"
+import Search from "./_components/search"
 
 const Home = async () => {
   const barbershops = await db.barbershop.findMany({})
@@ -27,11 +28,9 @@ const Home = async () => {
         <p>Segunda-feira, 05 de agosto.</p>
 
         {/* BUSCA */}
-        <div className="mt-6 flex items-center gap-2">
+        <div className="mt-6">
           <Input placeholder="Faça sua busca..." />
-          <Button>
-            <SearchIcon />
-          </Button>
+          <Search />
         </div>
 
         {/* BUSCA RÁPIDA */}
