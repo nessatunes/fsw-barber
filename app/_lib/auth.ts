77da9117354e3870374815a1a5c50b0726fb0502
Nessatunes <essatunes@gmail.com>
2024-08-12 17:05:17 -0300

    chore: bookings details

--- app/_lib/auth.ts
+++ app/_lib/auth.ts
@@ -1,6 +1,6 @@
-import { db } from "@/app/_lib/prisma"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import { AuthOptions } from "next-auth"
+import { db } from "./prisma"
 import { Adapter } from "next-auth/adapters"
 import GoogleProvider from "next-auth/providers/google"
 
@@ -14,14 +14,11 @@ export const authOptions: AuthOptions = {
   ],
   callbacks: {
     async session({ session, user }) {
-      session.user = { ...session.user, id: user.id } as {
-        id: string
-        name: string
-        email: string
-      }
-
+      session.user = {
+        ...session.user,
+        id: user.id,
+      } as any
       return session
     },
   },
-  secret: process.env.NEXT_AUTH_SECRET,
 }
